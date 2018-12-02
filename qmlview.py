63aa6417d19e246e3308e7d7b09aa7df21262a66
amoh-godwin <amohgyebigodwin@gmail.com>
2018-12-02 15:00:00 +0000

    debugging: Qt exits with right status code

--- qmlview.py
+++ qmlview.py
@@ -3,17 +3,16 @@ import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
-
 def run():
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
+    
     engine.load(sys.argv[1])
-    sys.exit(app.exec_())
 
-    return 0
-
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
 
 if len(sys.argv) > 1:
     run()
 else:
     print('Usage: qmlview file')
+
+sys.exit(app.exec_())
