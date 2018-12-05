8c5d64530da5a37489ae166c4b7b6731b635e1eb
amoh-godwin <amohgyebigodwin@gmail.com>
2018-12-05 22:21:33 +0000

    add icon

--- qmlview.py
+++ qmlview.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtGui import QGuiApplication, QIcon
 from PyQt5.QtQml import QQmlApplicationEngine
 
+
 def run():
-    
+
     engine.load(sys.argv[1])
 
+
 app = QGuiApplication(sys.argv)
+app.setWindowIcon(QIcon('./resources/icons/ic_airplay_white_18dp.png'))
 engine = QQmlApplicationEngine()
 
 if len(sys.argv) > 1:
     run()
 else:
-    print('Usage: qmlview file')
+    print('Usage: qmlview file or ./qmlview file')
 
 sys.exit(app.exec_())
