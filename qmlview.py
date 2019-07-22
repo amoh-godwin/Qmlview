cd9a614b86e798619cb03eb1b8547a001626efbd
amoh-godwin <amohgyebigodwin@gmail.com>
2019-07-22 16:31:10 +0000

    change icon logo

--- qmlview.py
+++ qmlview.py
@@ -3,15 +3,15 @@ import sys
 from PyQt5.QtGui import QGuiApplication, QIcon
 from PyQt5.QtQml import QQmlApplicationEngine
 
+app = QGuiApplication(sys.argv)
+app.setWindowIcon(QIcon('./resources/icons/logo.png'))
+engine = QQmlApplicationEngine()
+
 
 def run():
 
     engine.load(sys.argv[1])
 
-
-app = QGuiApplication(sys.argv)
-app.setWindowIcon(QIcon('./resources/icons/ic_airplay_white_18dp.png'))
-engine = QQmlApplicationEngine()
 
 if len(sys.argv) > 1:
     run()
