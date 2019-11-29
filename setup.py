8fd63fcb3d04ab7bbcc9929e4a6bdee7a5dfb42e
amoh-godwin <amohgyebigodwin@gmail.com>
2019-11-29 11:33:11 +0000

    change to beta befor using real online data

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="Qmlview",
-    version="1.0-beta",
+    version="0.3-beta",
     packages=find_packages(),
     install_requires=['PyQt5 >= 5.10'],
     entry_points={
@@ -21,7 +21,7 @@ setup(
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
