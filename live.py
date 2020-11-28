f0f1894da5fd56c4f405074b9ad8d48a46e2671c
amoh-godwin <amohgyebigodwin@gmail.com>
2020-11-28 13:03:32 +0000

    Add skeleton module for live reloading

---
+++ live.py
@@ -0,0 +1,14 @@
+"""
+Module for Live Reloading
+"""
+from PyQt5.QtCore import QFile, QResource, QIODevice
+QResource.registerResource("_qmlview_resource_.rcc")
+
+
+class Live():
+
+
+    """
+    """
+
+    pass
